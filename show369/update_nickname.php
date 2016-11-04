ded044e3cf833614997b134bb24cb3b1fdf47ac6
neo1seok <neo1seok@gmail.com>
2016-11-04 15:06:07 +0900

    change12

--- show369/update_nickname.php
+++ show369/update_nickname.php
@@ -3,14 +3,15 @@ include("library.php");  // library.php 파일 포함
 
 $nck_uid = $_REQUEST['id'];
 $nickname = $_REQUEST['nickname'];
+$stamp = $_REQUEST['stamp'];
 echo $nickname; 
 
 if($nickname == ''){
-	alertBoxAndBack('nickname을 채워 주십 시오. ');
-	exit();
+//	alertBoxAndBack('nickname을 채워 주십 시오. ');
+//	exit();
 }
 
-$sql = "UPDATE nickname SET nickname = '$nickname' where nck_uid = '$nck_uid' ;"		;
+$sql = "UPDATE nickname SET nickname = '$nickname' ,stamp=$stamp where nck_uid = '$nck_uid' ;"		;
 
 echo $sql;
 pnl();
