6f2e9a670ceb62f867f3e7f837d434ac43ee5bfd
neo1seok <neo1seok@gmail.com>
2017-03-19 18:27:27 +0900

    a

--- webtoon/webtoon.php
+++ webtoon/webtoon.php
@@ -284,6 +284,29 @@ $totallist = viewWebtoonLink_total();
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+		<style type="text/css">
+
+		ul {
+			list-style-type: none;
+			padding-right: 20px;
+		padding-left: 0px;
+		}
+
+
+		li {
+		 display: inline;
+		 padding: 0 20px;
+		 border-right:  1px solid #999;
+		}
+		li[class=\"st\"]{
+		 display: inline;
+		 padding: 0 10px;
+		}
+		.st {
+		 display: inline;
+		 padding: 0 10px;
+		}
+		</style>
   </head>
 </head>
 <body>
@@ -295,6 +318,7 @@ $totallist = viewWebtoonLink_total();
         <p><?php echo $header; ?></p>
         <p><a class="btn btn-info btn-lg" id='toggle_today' href="#" role="button">오늘의 웹툰 »</a></p>
 				<p><a class="btn btn-info btn-lg" id='toggle_all' href="#" role="button">전체 목록 »</a></p>
+				<p><a class="btn btn-info btn-lg" id='toggle_add_webtoon' href="#" role="button">웹툰추가 »</a></p>
       </div>
     </div>
 
@@ -341,7 +365,18 @@ $totallist = viewWebtoonLink_total();
 
 				$('#div_all').toggle();
 
+
+
 			});
+			$('#toggle_add_webtoon').click(function(){
+				console.log('toggle_all');
+
+				$('#div_input').toggle();
+
+
+
+			});
+			//div_input
 
     });
 
@@ -377,6 +412,19 @@ $totallist = viewWebtoonLink_total();
 		 </tbody>
 		</table>
 		</div>
+		<div class="col-md-6" id='div_input' >
+			<h2>웹툰 입력</h2>
 
+			<form class="navbar-form navbar-right">
+		 	 <div class="form-group">
+		 		 <input type="text" placeholder="웹툰아이디" class="form-control">
+		 	 </div>
+			 <div class="form-group">
+				 <input type="text" placeholder="연재요일" class="form-control">
+			 </div>
+		  	 <button type="submit" id='btn_add_webtoon' class="btn btn-success">입력</button>
+		  </form>
+
+		</div>
 </body>
 </html>
