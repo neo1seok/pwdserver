5ce0721f54acaac445f61f6ee9c6a3a7eaa01be2
neo1seok <neo1seok@gmail.com>
2017-05-30 13:51:20 +0900

    db

--- pwdserver/showlist.php
+++ pwdserver/showlist.php
@@ -16,31 +16,33 @@ $incondition = '';
 
 if($uids != 'all')
 {
-	
+
 	$uidarray = explode (",", $uids);
 	$i = 0;
 	foreach ($uidarray as &$v) {
 		$v = "'".$v."'";
-	
+
 	}
 	$strarrays = join(',', $uidarray);
 	$incondition = 'and pwd_uid in ('.$strarrays.')';
 }
 
-		
+
 $mapresult = QueryString2Map("SELECT pwd_uid,site, B.title,ptail,B.phd_uid FROM passwd A,pheader B where A.phd_uid = B.phd_uid $incondition;");
 
 viewQueryResult($mapresult);
 defLink();
-?>	
+?>
 
-
+<form method='post' action='search.php'>
+<table>
+<tr>
+	<td><input type='text' name='keyword' tabindex='1'/></td>
+	<td><input type='submit' tabindex='2' value='검색'/></td>
+</tr>
+</table>
+</form>
 <!--
 <p><a href='main.php'>매인화면</a></p>
 <p><a href='logout.php'>로그아웃</a></p>
 -->
-
-
-
-
-
