2ee2f511d840da723cae2810bc3204eb536cd89b
neo1seok <neo1seok@gmail.com>
2017-07-04 10:51:46 +0900

    fix db input

--- neocontents/main.php
+++ neocontents/main.php
@@ -205,7 +205,8 @@ $list_contents = json_encode(QueryString2Map($sql));
 
 		</div>
     <button class="w3-btn w3-green w3-ripple"  ng-click="newcontents()" >&#10004;  새 글쓰기</button>
-    <button class="w3-btn w3-green w3-ripple"  ng-click="test()" >&#10004; TEST</button>
+    <br>
+    <!-- <button class="w3-btn w3-green w3-ripple"  ng-click="test()" >&#10004; TEST</button> -->
 
     <form ng-show="shwoContents" class="w3-container" id="div_input" name="div_input">
       <h3>{{contents_title}}</h2>
