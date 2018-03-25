0c2e783a9ab6d981970a76107ea20a587f575b47
neo1seok <neo1seok@gmail.com>
2018-03-25 10:58:19 +0900

    aa

--- test.php
+++ test.php
@@ -1,21 +1,25 @@
 <?php
-require_once("library.php");  // library.php 파일 포함
-//defMeta();
-setHome("");
 
-$maplist = QueryString2Map("SELECT seq, fnk_uid, title, link, updt_date, reg_date, comment FROM fav_link where type = 'MAIN' order by seq;");
-//$maplist = QueryString2Map("SELECT seq, fnk_uid, title, link, updt_date, reg_date, comment FROM fav_link  order by seq;");
-// $list_aa = array();
 
-foreach ($maplist as $map){
-	$title = $map['title'];
-	$link = $map['link'];
-	$list_bb = array();
-	array_push($list_bb, $title, $link);
-	array_push($list_aa, $list_bb);
-	echo "<p><a href='$link'>$title</a></p>";
-	echo "\n";
+$dbname = "neo_pwinfo";
 
-}
+//Connect DB
+$connect = @mysql_connect("localhost:3306","neo1seok","tofhdna1pi") or die("DB접속에러");
 
+//Select DB
+@mysql_select_db($dbname,$connect) or die("DB선택에러");
+
+$strQuery = "SELECT seq, fnk_uid, title, link, updt_date, reg_date, comment FROM fav_link where type = 'MAIN' order by seq;"
+
+$result = @mysql_query($strQuery) or die($die_string(mysql_error(),$strQuery));
+
+$data=@mysql_fetch_row($result);
+
+mysql_close($connect);
+
+$map = $data[0];
+$title = $map['title'];
+$link = $map['link'];
+
+echo $title;
 ?>
